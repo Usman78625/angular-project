98a78189f9ce51e1ea28964ea394040a127118af
Usman123 <um7995367@gmail.com>
2023-06-01 20:02:28 +0500

    Adding content to recpie component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+recipes: Recipe[] = [
+  new Recipe('A test recipe', 'This is simply a test','https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_1600,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg')
+];
 
+constructor(){
+
+}
 }
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagepath: string;
+  
+    constructor(name: string, desc: string, imagepth: string) {
+      this.name = name;
+      this.description = desc;
+      this.imagepath = imagepth;
+    }
+  }
+  
